db3c4e4e0b21b93f745e37cea9c7df4d637c9790
Grant Windes <gwindes@gmail.com>
2020-12-17 23:37:01 -0800

    Swapped guitar up.down to be default on and go to an off mode for the riff

--- sequences/deck-the-halls_war-pigs.py
+++ sequences/deck-the-halls_war-pigs.py
@@ -1,18 +1,12 @@
 guitar_sleep = 0.1
-#GPIO.output(ALL, GPIO.HIGH)
+# GPIO.output(ALL, GPIO.HIGH)
+# TURN ON ALL
 GPIO.output(C9, GPIO.LOW)
-
-# riff up
-GPIO.output(REINDEER_STABLES, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(ELVES_BUNK, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(TREE, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(SANTA_HOUSE, GPIO.LOW)
-sleep(rift_sleep)
 GPIO.output(POST_OFFICE, GPIO.LOW)
-sleep(rift_sleep)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+GPIO.output(TREE, GPIO.LOW)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
 
 # riff down
 GPIO.output(POST_OFFICE , GPIO.HIGH)
@@ -24,6 +18,18 @@ sleep(rift_sleep)
 GPIO.output(ELVES_BUNK, GPIO.HIGH)
 sleep(rift_sleep)
 GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(rift_sleep)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(TREE, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(POST_OFFICE, GPIO.LOW)
 sleep(rift_sleep)
 
 # lyrics background symbol | 0:01 - 0:05
@@ -33,18 +39,6 @@ for _ in range(50):
     GPIO.output(C9, GPIO.HIGH)
     sleep(0.1)
 
-# riff up
-GPIO.output(REINDEER_STABLES, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(ELVES_BUNK, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(TREE, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(SANTA_HOUSE, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(POST_OFFICE, GPIO.LOW)
-sleep(rift_sleep)
-
 # riff down
 GPIO.output(POST_OFFICE , GPIO.HIGH)
 sleep(rift_sleep)
@@ -55,6 +49,18 @@ sleep(rift_sleep)
 GPIO.output(ELVES_BUNK, GPIO.HIGH)
 sleep(rift_sleep)
 GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(rift_sleep)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(TREE, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(POST_OFFICE, GPIO.LOW)
 sleep(rift_sleep)
 
 # lyrics background symbol  | 0:06 - 0:10
@@ -64,18 +70,6 @@ for _ in range(50):
     GPIO.output(C9, GPIO.HIGH)
     sleep(0.1)
 
-# riff up
-GPIO.output(REINDEER_STABLES, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(ELVES_BUNK, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(TREE, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(SANTA_HOUSE, GPIO.LOW)
-sleep(rift_sleep)
-GPIO.output(POST_OFFICE, GPIO.LOW)
-sleep(rift_sleep)
-
 # riff down
 GPIO.output(POST_OFFICE , GPIO.HIGH)
 sleep(rift_sleep)
@@ -86,6 +80,18 @@ sleep(rift_sleep)
 GPIO.output(ELVES_BUNK, GPIO.HIGH)
 sleep(rift_sleep)
 GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(rift_sleep)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(TREE, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(rift_sleep)
+GPIO.output(POST_OFFICE, GPIO.LOW)
 sleep(rift_sleep)
 
 # lyrics background symbol  | 0:11 - 0:16
@@ -95,6 +101,17 @@ for _ in range(50):
     GPIO.output(C9, GPIO.HIGH)
     sleep(0.1)
 
+# riff down
+GPIO.output(POST_OFFICE , GPIO.HIGH)
+sleep(rift_sleep)
+GPIO.output(SANTA_HOUSE, GPIO.HIGH)
+sleep(rift_sleep)
+GPIO.output(TREE, GPIO.HIGH)
+sleep(rift_sleep)
+GPIO.output(ELVES_BUNK, GPIO.HIGH)
+sleep(rift_sleep)
+GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+
 # riff up
 GPIO.output(REINDEER_STABLES, GPIO.LOW)
 sleep(rift_sleep)
@@ -106,15 +123,4 @@ GPIO.output(SANTA_HOUSE, GPIO.LOW)
 sleep(rift_sleep)
 GPIO.output(POST_OFFICE, GPIO.LOW)
 sleep(rift_sleep)
-
-# riff down
-GPIO.output(POST_OFFICE , GPIO.HIGH)
-sleep(rift_sleep)
-GPIO.output(SANTA_HOUSE, GPIO.HIGH)
-sleep(rift_sleep)
-GPIO.output(TREE, GPIO.HIGH)
-sleep(rift_sleep)
-GPIO.output(ELVES_BUNK, GPIO.HIGH)
-sleep(rift_sleep)
-GPIO.output(REINDEER_STABLES, GPIO.HIGH)
 sleep(rift_sleep)

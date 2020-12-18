0f14ed9f2567e93489e4f4e06004ab013dc28640
Grant Windes <gwindes@gmail.com>
2020-12-17 23:19:38 -0800

    First initial pass at sequences

---
+++ sequences/deck-the-halls_war-pigs.py
@@ -0,0 +1,119 @@
+GPIO.output(ALL, GPIO.HIGH)
+GPIO.output(C9, GPIO.LOW)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(0.2)
+GPIO.output(TREE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(POST_OFFICE, GPIO.LOW)
+sleep(0.2)
+
+# riff down
+GPIO.output(POST_OFFICE , GPIO.HIGH)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(TREE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(0.2)
+
+# lyrics background symbol | 0:01 - 0:05
+for _ in range(50):
+    GPIO.output(C9, GPIO.LOW)
+    sleep(0.1)
+    GPIO.output(C9, GPIO.HIGH)
+    sleep(0.1)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(0.2)
+GPIO.output(TREE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(POST_OFFICE, GPIO.LOW)
+sleep(0.2)
+
+# riff down
+GPIO.output(POST_OFFICE , GPIO.HIGH)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(TREE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(0.2)
+
+# lyrics background symbol  | 0:06 - 0:10
+for _ in range(50):
+    GPIO.output(C9, GPIO.LOW)
+    sleep(0.1)
+    GPIO.output(C9, GPIO.HIGH)
+    sleep(0.1)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(0.2)
+GPIO.output(TREE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(POST_OFFICE, GPIO.LOW)
+sleep(0.2)
+
+# riff down
+GPIO.output(POST_OFFICE , GPIO.HIGH)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(TREE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(0.2)
+
+# lyrics background symbol  | 0:11 - 0:16
+for _ in range(50):
+    GPIO.output(C9, GPIO.LOW)
+    sleep(0.1)
+    GPIO.output(C9, GPIO.HIGH)
+    sleep(0.1)
+
+# riff up
+GPIO.output(REINDEER_STABLES, GPIO.LOW)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.LOW)
+sleep(0.2)
+GPIO.output(TREE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.LOW)
+sleep(0.2)
+GPIO.output(POST_OFFICE, GPIO.LOW)
+sleep(0.2)
+
+# riff down
+GPIO.output(POST_OFFICE , GPIO.HIGH)
+sleep(0.2)
+GPIO.output(SANTA_HOUSE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(TREE, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(ELVES_BUNK, GPIO.HIGH)
+sleep(0.2)
+GPIO.output(REINDEER_STABLES, GPIO.HIGH)
+sleep(0.2)
